e0608ca6e17ff65a7f0026256eb142bb99db5ef4
kelen940511 <88041456+KelenHappy@users.noreply.github.com>
2024-04-03 16:39:08 +0800

    update

--- chess.py
+++ chess.py
@@ -10,39 +10,37 @@ class const:
        
 const.MARGIN = 40
 const.GRID = 25
-const.NUM = 22
-const.LEN = 525
+const.NUM = 23
+const.LEN = 550
 const.WIN = 3
 
 class Chess:
     def __init__(self):
         self.cord = []        
         self.size = 0
-    def setXY(self,x: int, y: int, player: int)->bool:
+    def isValidCandidate(self,x: int, y: int, player: int)->bool:
         if x==0 or y==0: return False
+        if x==22 or y==22: return False
         data = [(d[0], d[1]) for d in self.cord]
-        if (x, y) not in data:
-            self.cord.append((x, y, player))
-            self.size += 1
-            return True
-        return False
+        if (x, y) in data: return False
+        return True
+    def setXY(self,x: int, y: int, player: int)->bool:
+        if self.isValidCandidate(x, y, player)==False: return False
+        self.cord.append((x, y, player))
+        self.size += 1
+        return True
     def printChess(self):
         for d in self.cord:
             print(d, end=' ')
         print('size=', self.size)   
         
 def drawBoard():
-    img = np.zeros((600,600,3), dtype='uint8')   # 繪製 300x300 的黑色畫布
-    for i in range(const.NUM):
-        cv2.line(img,(const.MARGIN, const.MARGIN+ i*const.GRID),(const.MARGIN+const.LEN, const.MARGIN + i*const.GRID),(0,255,0),1)  # 繪製線條
-        cv2.line(img,(const.MARGIN+ i*const.GRID, const.MARGIN),(const.MARGIN + i*const.GRID, const.MARGIN+const.LEN),(0,255,0),1)  # 繪製線條        
+    img = np.zeros((700,700,3), dtype='uint8')   # 繪製 300x300 的畫布
+    img[:] = (118, 208, 255)                     # 改成淡黃色   
+    for i in range(const.NUM):                   # 棋盤黑線
+        cv2.line(img,(const.MARGIN, const.MARGIN+ i*const.GRID),(const.MARGIN+const.LEN, const.MARGIN + i*const.GRID),(0,0,0),1)  # 繪製線條
+        cv2.line(img,(const.MARGIN+ i*const.GRID, const.MARGIN),(const.MARGIN + i*const.GRID, const.MARGIN+const.LEN),(0,0,0),1)  # 繪製線條        
     return img
-
-def writeBack(file: str, x:int, y: int):
-    f = open(file,'w')
-    f.write('W\n')
-    f.write(str(x)+' '+str(y))
-    f.close()    
 
 def checkWin(data: list):
     count=0
@@ -120,7 +118,7 @@ def checkBoardSlashRL(board: list):
             return True               
     return False
     
-def gameOver(chess: Chess, player: int):
+def isGameOver(chess: Chess, player: int):
     # 取出 chess 物件紀錄的 x, y 座標
     data = [(d[0], d[1]) for d in chess.cord if d[2]==player]
     # 初始化 N*N board 矩陣棋盤，設定為 0
@@ -148,52 +146,86 @@ def testGameOver():
     print(flag, player)    
 
 #testGameOver()    
-    
-def go(img, fileName: str, px: int, py: int, c: Chess, player: int):
-    if player == 0: color = (0, 0, 255)
-    else: color = (255, 0, 0)   
-        
-    f = open(fileName,'r')
-    k = f.readline()
-    if k[0]=='R':
+
+def readCord(fileName: str):
+    while True:
+        f = open(fileName,'r')
         k = f.readline()
-        data = k.split()
-        x, y = int(data[0]), int(data[1])
-        print('==>', x, y)
-        c.setXY(x, y, player)
-        c.printChess()
-        cv2.circle(img, (const.MARGIN + x*const.GRID, const.MARGIN + y*const.GRID), 10, color, -1)
+        if k[0]=='R':
+            k = f.readline()
+            data = k.split()
+            x, y = int(data[0]), int(data[1])
+            print('==>', x, y)
+            f.close()
+            return x, y
         f.close()
-        writeBack(fileName, px, py)
-        return True, x, y
-    f.close()
-    return False, 0, 0
+        cv2.waitKey(3000)
+    return 0, 0
+
+def writeBack(file: str, x:int, y: int):
+    f = open(file,'w')
+    f.write('W\n')
+    f.write(str(x)+' '+str(y))
+    f.close()    
+    
+def onePlayerGo(player: int, fileName: str, img, c:Chess, color)->bool:
+    file1, file2 = const.fileA, const.fileB
+    if player==1: file1, file2 = const.fileB, const.fileA
+    x, y = readCord(file1)
+    if c.isValidCandidate(x, y, player)==False: return False
+    cv2.circle(img, (const.MARGIN + x*const.GRID, const.MARGIN + y*const.GRID), 10, color, -1)
+    c.setXY(x, y, player)
+    writeBack(file2, x, y)
+    cv2.imshow("image", img)
+    cv2.waitKey(2000)
+    return True
+    
+def go(img, c: Chess):
+    if onePlayerGo(0, const.fileA, img, c, (0, 0, 0))==False: return 1
+    if onePlayerGo(1, const.fileB, img, c, (255, 255, 255))==False: return 0  
+    c.printChess()
+    return 2
 
 def main():
+    winner = 'A'
+    const.fileA = input('fileName A: ')
+    const.fileB = input('fileName B: ')
+    writeBack(const.fileA, 0, 0)
+    writeBack(const.fileB, 0, 0)
     chess = Chess()
     img = drawBoard()
-    fileName = 'a.txt'
-    px, py = 0, 0    
+
     cv2.imshow("image", img)
     while True:        
-        if fileName == 'a.txt': player = 0
-        else: player = 1
-        change, px, py = go(img, fileName, px, py, chess, player)                               
-        over = gameOver(chess, player)
-        if over == True:
-            print(over, player)
+        over = go(img, chess)
+        if over==0:
+            print('Over', 0)
+            winner = 'A'
+            break
+        elif over==1:
+            print('Over', 1)
+            winner = 'B'
+            break
+        overA = isGameOver(chess, 0)
+        overB = isGameOver(chess, 1)
+        if overA == True:
+            print(overA, 0)
+            winner = 'A'
+            break
+        if overB == True:
+            print(overB, 1)
+            winner = 'B'
             break
         if cv2.waitKey(1000)==27:
-            break        
-        if change==True and fileName=='a.txt': 
-            fileName = 'b.txt'
-        elif change==True and fileName=='b.txt': 
-            fileName = 'a.txt'
-        if change == True:
-            cv2.imshow("image", img)
+            break      
         
+    text = 'Winner ' + winner
+    cv2.putText(img, text, (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
     cv2.imshow("image", img)
-    cv2.waitKey(18000)
+    while True:                      
+        if cv2.waitKey(1000)==27:
+            break        
+    #cv2.waitKey(2000)
     cv2.destroyAllWindows()            
 
 main()    
